9c9ed73ea13f2af6c6618f0b61050c3d29c3d64b
Sven <svenrisse0@gmail.com>
2024-03-30 14:36:37 +0100

    feat: add list endpoint without db yet

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -14,6 +14,8 @@ func (app *application) routes() http.Handler {
 	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
 
 	router.HandlerFunc(http.MethodGet, "/v1/health", app.healthcheckHandler)
+
+	router.HandlerFunc(http.MethodGet, "/v1/movies", app.listMovieHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
 	router.HandlerFunc(http.MethodPatch, "/v1/movies/:id", app.updateMovieHandler)
