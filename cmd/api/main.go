135c38058f43b30fafc0c0188db821077fb3959d
Sven <svenrisse0@gmail.com>
2024-04-05 13:09:26 +0200

    feat: generate version number

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"expvar"
 	"flag"
+	"fmt"
 	"log/slog"
 	"os"
 	"runtime"
@@ -17,9 +18,10 @@ import (
 	_ "github.com/lib/pq"
 	"github.com/svenrisse/greenlight/internal/data"
 	"github.com/svenrisse/greenlight/internal/mailer"
+	"github.com/svenrisse/greenlight/internal/vcs"
 )
 
-const version = "1.0.0"
+var version = vcs.Version()
 
 type config struct {
 	port int
@@ -91,7 +93,14 @@ func main() {
 		return nil
 	})
 
+	displayVersion := flag.Bool("version", false, "Display version and exit")
+
 	flag.Parse()
+
+	if *displayVersion {
+		fmt.Printf("Version:\t%s\n", version)
+		os.Exit(0)
+	}
 
 	db, err := openDB(cfg)
 	if err != nil {
